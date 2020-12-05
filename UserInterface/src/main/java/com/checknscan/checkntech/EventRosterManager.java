64bfeeeec8572069791260063e4d1386068a2637
aduncan9 <74635861+aduncan9@users.noreply.github.com>
2020-12-04 22:18:49 -0600

    Fixed a comment.

--- UserInterface/src/main/java/com/checknscan/checkntech/EventRosterManager.java
+++ UserInterface/src/main/java/com/checknscan/checkntech/EventRosterManager.java
@@ -156,7 +156,7 @@ public class EventRosterManager extends javax.swing.JFrame {
                 eventObjRoster[i][0] = eventRoster[i][0];
                 eventObjRoster[i][1] = eventRoster[i][1];
                 eventObjRoster[i][2] = eventRoster[i][2];
-                System.out.println("Roster Length: " + eventRoster[i].length);
+                // System.out.println("Roster Length: " + eventRoster[i].length);
                 if (eventRoster[i][3].equals("1")) {
                 // if (DBConnector.isFirstCheckNull((String) eventObjRoster[i][0], getSelectedEventID()) == null) {
                     eventObjRoster[i][3] = "YES"; //eventRoster[i][3];
